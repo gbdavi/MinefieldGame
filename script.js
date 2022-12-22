2abff1ab0c9e49904242e1b33a49ca739f93be3f
DaviSenai <davi_bauer@estudante.sesisenai.org.br>
2022-12-22 11:23:54 -0300

    v0.5

--- script.js
+++ script.js
@@ -5,36 +5,104 @@ class Tile {
     valueElem;
     popupElem;
     flagElem;
-    value;
+    value = undefined;
     revealed = false;
     flagAssigned = false;
     x;
     y;
 
-    constructor(x, y, value) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.value = value;
         this.genModel();
         this.elem.onclick = () => {
-            // this.valueElem.style.visibility = "visible";
-            Table.removeAllSelected();
-            if (!this.revealed)
-            this.popupElem.style.visibility = "visible";
+            if (this.value == undefined) {
+                Table.setBombs(this.x, this.y);
+                Table.setIndicators();
+                this.reveal();
+            } else {
+                Table.hidePopUps();
+                if (!this.revealed)
+                    this.showPopUp()
+                
+            }
+        }
+        this.elem.onauxclick = (e) => {
+            if ( !this.revealed ) {
+                e.stopImmediatePropagation();
+                this.switchFlag();
+                Table.hidePopUps();
+            }
         }
     }
 
     genModel() {
-        let elem = document.createElement("div");
+        this.elem = document.createElement("div");
 
-        let valueContainer = document.createElement("div");
-        valueContainer.innerText = this.value;
-        valueContainer.classList.add("content");
+        this.valueElem = document.createElement("div");
+        this.valueElem.classList.add("content");
+        this.elem.appendChild( this.valueElem );
 
+        this.flagElem = document.createElement("img");
+        this.flagElem.src = "./icons/flag.png";
+        this.flagElem.classList.add("flag");
+        this.elem.appendChild(this.flagElem);  
+        
+        this.popupElem = document.createElement("div");
+        this.popupElem.classList.add("pop-up-container");
+
+        const container1 = document.createElement("div");
+        {
+            const showelContainer = document.createElement("div");
+            showelContainer.classList.add("showel-container");
+            showelContainer.innerHTML += '<img src="./icons/showel.png">';
+            showelContainer.addEventListener("click", (e) => {
+                // Impede o click do elemento pai ser executado
+                e.stopImmediatePropagation();
+                this.reveal();
+                this.revealed = true;
+                Table.hidePopUps();
+            });
+            container1.appendChild( showelContainer );
+
+            const flagContainer = document.createElement("div");
+            flagContainer.classList.add("flag-container");
+            flagContainer.innerHTML += '<img src="./icons/flag.png">';
+            flagContainer.addEventListener("click", (e) => {
+                // Impede o click do elemento pai ser executado
+                e.stopImmediatePropagation();
+                this.switchFlag();
+                Table.hidePopUps();
+            });
+            container1.appendChild( flagContainer );
+        }
+        this.popupElem.appendChild(container1);
+
+        const container2 = document.createElement('div');
+        {
+            const closeContainer = document.createElement("div");
+            closeContainer.classList.add("close-container");
+            closeContainer.innerHTML += '<img src="./icons/close.png">';
+            closeContainer.addEventListener("click", (e) => {
+                // Impede o click do elemento pai ser executado
+                e.stopImmediatePropagation();
+                Table.hidePopUps();
+            });
+            container2.appendChild( closeContainer );
+            container2.appendChild( document.createElement("div") );
+        }
+        this.popupElem.appendChild(container2);
+
+        this.elem.appendChild( this.popupElem );
+    }
+
+    setValue(value) {
+        this.value = value;
+        this.valueElem.innerText = value;
         let color;
         switch(this.value) {
             case -2:
-                valueContainer.innerHTML = `<img src="./icons/explosion.png">`;
+                this.valueElem.innerHTML = `<img src="./icons/explosion.png">`;
                 color = "transparent";
                 break;
             case 1: color = "#961189"; break;
@@ -47,67 +115,7 @@ class Tile {
             case 8: color = "#000000"; break;
             default: color = "transparent";
         }
-        valueContainer.style.color = color;
-        elem.appendChild( valueContainer );
-
-        this.flagElem = document.createElement("img");
-        this.flagElem.src = "./icons/flag.png";
-        this.flagElem.classList.add("flag");
-        elem.appendChild(this.flagElem);
-        
-        
-        let popupContainer = document.createElement("div");
-        popupContainer.classList.add("pop-up-container");
-
-        const container1 = document.createElement("div");
-
-            const showelContainer = document.createElement("div");
-            showelContainer.classList.add("showel-container");
-            showelContainer.innerHTML += '<img src="./icons/showel.png">';
-            showelContainer.addEventListener("click", (e) => {
-                // Impede o click do elemento pai ser executado
-                e.stopImmediatePropagation();
-                this.reveal();
-                this.revealed = true;
-                Table.removeAllSelected();
-            });
-
-            const flagContainer = document.createElement("div");
-            flagContainer.classList.add("flag-container");
-            flagContainer.innerHTML += '<img src="./icons/flag.png">';
-            flagContainer.addEventListener("click", (e) => {
-                // Impede o click do elemento pai ser executado
-                e.stopImmediatePropagation();
-                this.switchFlag();
-                Table.removeAllSelected();
-            });
-        
-        container1.appendChild( showelContainer );
-        container1.appendChild( flagContainer );
-
-        const container2 = document.createElement('div');
-
-            const closeContainer = document.createElement("div");
-            closeContainer.classList.add("close-container");
-            closeContainer.innerHTML += '<img src="./icons/close.png">';
-            closeContainer.addEventListener("click", (e) => {
-                // Impede o click do elemento pai ser executado
-                e.stopImmediatePropagation();
-                Table.removeAllSelected();
-            });
-
-        container2.appendChild( closeContainer );
-        container2.appendChild( document.createElement("div") );
-        
-        
-        popupContainer.appendChild(container1);
-        popupContainer.appendChild(container2);
-
-        elem.appendChild( popupContainer );
-        
-        this.elem = elem;
-        this.valueElem = valueContainer;
-        this.popupElem = popupContainer;
+        this.valueElem.style.color = color;
     }
 
     reveal() {
@@ -119,12 +127,10 @@ class Tile {
             this.elem.querySelector(".flag").style.visibility = "";
 
             if (this.value == 0) {
-                console.log(this.value)
                 Table.revealRound(this.x, this.y);
             }
             if (this.value == -2) {
-                
-                Table.revealBombs();
+                Table.revealBombs(this);
             }
         }
     }
@@ -133,143 +139,141 @@ class Tile {
         if (!this.flagAssigned) {
             this.flagAssigned = true;
             this.flagElem.style.visibility = "visible";
+            Table.nFlags--;
         } else {
             this.flagAssigned = false;
             this.flagElem.style.visibility = "hidden";
+            Table.nFlags++;
         }
     }
+
+    showPopUp() {
+        this.popupElem.style.visibility = "visible";
+    }
+
+    hidePopUp() {
+        this.popupElem.style.visibility = "";
+    }
+    
 }
 
 
+// Gerar Table.grid vazia (value = undefined)
+
 const Table =  {
     nBombs: 35,
+    nFlags: this.nBombs,
     gridSizeX: 18,
     gridSizeY: 11,
-    table: document.querySelector("#table-container"),
+    elem: document.querySelector("#table-container"),
     grid:[],
     bombs:[],
 
-    start() {
-        this.table.innerHTML = "";
-        let tiles = Table.genBombs();
-        Table.genTable(
-            Table.addIndicators(
-                tiles
-            )
-        );
-        for (let col = 0; col < tiles.length; col++) {
-            for (let row = 0; row < tiles[col].length; row++) {
-                if (Table.grid[row][col].value == -2) {
-                    Table.bombs.push( Table.grid[row][col] )
-                    // Table.bombs.push( Table.table.querySelectorAll(".row > div")[(col + row*Table.gridSizeX)] )
-                }
-            }
-        }
-    },
-
-    genTable(table) {
-        for (let i = 0; i < Table.gridSizeY; i++) {
+    genTable() {
+        Table.grid = [];
+        for (let rowPos = 0; rowPos < Table.gridSizeY; rowPos++) {
             let gridRow = [];
-            let row = document.createElement("div");
-            row.classList.add("row");
-            for (let j = 0; j < Table.gridSizeX; j++) {
-                gridRow.push( new Tile(j, i, table[j][i]) );
-                row.appendChild( gridRow[j].elem );
+            for (let colPos = 0; colPos < Table.gridSizeX; colPos++) {
+                gridRow.push( new Tile(colPos, rowPos) );
             }
-            this.table.appendChild( row );
             Table.grid.push( gridRow );
         }
     },
 
-    random() {
-        let x, y;
-        do {
-            x = Math.floor( Math.random()*100 );
-        } while (x >= this.gridSizeX);
-
-        do {
-            y = Math.floor( Math.random()*100 );
-        } while (y >= this.gridSizeY);
-
-        return {x, y};
+    createTable() {
+        Table.genTable();
+        Table.elem.innerHTML = "";
+        for (let rowPos = 0; rowPos < Table.gridSizeY; rowPos++) {
+            let row = document.createElement("div");
+            row.classList.add("row");
+            for (let colPos = 0; colPos < Table.gridSizeX; colPos++) {
+                row.appendChild( Table.grid[rowPos][colPos].elem );
+            }
+            Table.elem.appendChild( row );
+        }
     },
 
-    genBombs() {
-        let table = Array();
-        for (let i = 0; i < Table.gridSizeX; i++) {
-            let array = Array();
-            for (let j = 0; j < Table.gridSizeY; j++) {
-                array.push(0);
-            }
-            table.push(array);
-        }
+    random() {
+        let row, col;
+        do {
+            row = Math.floor( Math.random()*100 );
+        } while (row >= this.gridSizeY);
 
+        do {
+            col = Math.floor( Math.random()*100 );
+        } while (col >= this.gridSizeX);
+
+        return {row, col};
+    },
+
+    setBombs(clickedX, clickedY) {
         let settedBombs = 0;
         for (settedBombs; settedBombs < Table.nBombs; settedBombs++) {
             let pos;
             do {
                 pos = Table.random();
-            } while (table[pos.x][pos.y] != 0);
-            table[pos.x][pos.y] = -2;
-        }
-        return table;
-    },
-
-    revealBombs(x, y) {
-        for (let i = 0; i < Table.bombs.length; i++) {
-            setTimeout( () => {
-                // Table.bombs[i].querySelector(".content").style.visibility = "visible";
-                // Table.bombs[i].querySelector(".flag").style.visibility = "";
-                Table.bombs[i].reveal();
-            }, i*500 );
+        } while ( Table.grid[pos.row][pos.col].value == -2 || !( pos.col < clickedX - 1 || pos.col > clickedX + 1 || pos.row < clickedY - 1 || pos.row > clickedY + 1 ) );
+            Table.grid[pos.row][pos.col].setValue(-2);
+            Table.bombs.push( Table.grid[pos.row][pos.col] );
         }
     },
 
-    addIndicators(table) {
+    revealBombs(bomb) {
+        console.log("bombs revealed!")
+        for (let i = 0, j = 0; i < Table.bombs.length; i++) {
+            if (Table.bombs[i] != bomb) {
+                setTimeout( () => {
+                    Table.bombs[i].reveal();
+                }, j*500 );
+                j++;
+            }
+        }
+    },
+
+    setIndicators() {
         for (let col = 0; col < Table.gridSizeX; col++) {
             for (let row = 0; row < Table.gridSizeY; row++) {
 
-                if (table[col][row] == -2) continue;
+                if (Table.grid[row][col].value == -2) continue;
 
                 let roundBombs = 0;
                 if (col-1 >= 0) {
-                    roundBombs += table[col-1][row] == -2 ? 1 : 0;
+                    roundBombs += Table.grid[row][col-1].value == -2 ? 1 : 0;
 
                     if (row-1 >= 0) {
-                        roundBombs += table[col-1][row-1] == -2 ? 1 : 0;
+                        roundBombs += Table.grid[row-1][col-1].value == -2 ? 1 : 0;
                     }
                     
                     if (row+1 < Table.gridSizeY) {
-                        roundBombs += table[col-1][row+1] == -2 ? 1 : 0;
+                        roundBombs += Table.grid[row+1][col-1].value == -2 ? 1 : 0;
                     }
                     
                 }
                 
                 if (col+1 < Table.gridSizeX) {
-                    roundBombs += table[col+1][row] == -2 ? 1 : 0;
+                    roundBombs += Table.grid[row][col+1].value == -2 ? 1 : 0;
 
                     if (row-1 >= 0) {
-                        roundBombs += table[col+1][row-1] == -2 ? 1 : 0;
+                        roundBombs += Table.grid[row-1][col+1].value == -2 ? 1 : 0;
                     }
                     
                     if (row+1 < Table.gridSizeY) {
-                        roundBombs += table[col+1][row+1] == -2 ? 1 : 0;
+                        roundBombs += Table.grid[row+1][col+1].value == -2 ? 1 : 0;
                     }
 
                 }
                 
                 if (row-1 >= 0) {
-                    roundBombs += table[col][row-1] == -2 ? 1 : 0;
+                    roundBombs += Table.grid[row-1][col].value == -2 ? 1 : 0;
                 }
                 
                 if (row+1 < Table.gridSizeY) {
-                    roundBombs += table[col][row+1] == -2 ? 1 : 0;
+                    roundBombs += Table.grid[row+1][col].value == -2 ? 1 : 0;
                 }
 
-                table[col][row] = roundBombs
+                Table.grid[row][col].setValue( roundBombs );
             }
         }
-        return table;
     },
 
     revealRound(col, row) {
@@ -309,16 +313,17 @@ const Table =  {
         }
     },
 
-    removeAllSelected(e) {
-        let popups = document.querySelectorAll(".pop-up-container");
-        for (let i = 0; i < popups.length; i++) {
-            popups[i].style.visibility = "";
+    hidePopUps() {
+        for (let row = 0; row < Table.gridSizeY; row++) {
+            for (let col = 0; col < Table.gridSizeX; col++) {
+                Table.grid[row][col].hidePopUp();
+            }
         }
     }
 
 }
 
-Table.start();
+Table.createTable();
 
 // Refatorar código para utilizar o objeto (Tile) ao invés do HTML
 // Criar os métodos no próprio Tile
